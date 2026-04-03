c131a19a1ca5c88dd7e04347acec91d1b8f9b21b
Enrique Rubio <enrique.rubio.developer@gmail.com>
2026-04-03 21:08:42 +0200

    fix(node): register IpifyService in HeartbeatModule for NestJS DI
    - Added IpifyService to HeartbeatModule providers
    - HeartbeatHelper constructor: mandatory injection (removed optional ?)
    - node-runtime.ts: pass new IpifyService() when creating HeartbeatHelper manually

--- src/modules/heartbeat/heartbeat.module.ts
+++ src/modules/heartbeat/heartbeat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HeartbeatHelper } from './heartbeat';
 import { HeartbeatService } from './services/heartbeat.service';
+import { IpifyService } from '../shared/infrastructure/ipify.service';
 
 @Module({
-  providers: [HeartbeatHelper, HeartbeatService],
+  providers: [IpifyService, HeartbeatHelper, HeartbeatService],
   exports: [HeartbeatService],
 })
 export class HeartbeatModule {}
